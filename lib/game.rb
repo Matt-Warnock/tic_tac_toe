8cae17fe24c70122df75e3e293086ae5b4418299
MattW <mattnwarnock@yahoo.co.uk>
2022-03-21 16:06:49 +0000

    Play turn first test

---
+++ lib/game.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Game
+  def play_turn(player_id, coordinate)
+  end
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'game'
+
+RSpec.describe Game do
+  describe '#play_turn' do
+    xit 'records a game turn' do
+      expect { subject.play_turn(:x, 1) }
+        .to change { subject.grid[0] }
+        .from(nil).to(:x)
+    end
+  end
+end
